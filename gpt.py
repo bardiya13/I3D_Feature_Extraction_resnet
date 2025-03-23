d82b6b5d5eb57680d0d3514557c1504a131d84a6
bardiya13 <bardi.soltan@gmail.com>
2025-03-23 09:41:51 +0330

    tets hbhbjnckd

--- gpt.py
+++ gpt.py
@@ -1,32 +1,16 @@
-from pathlib import Path
-import shutil
-import argparse
-import numpy as np
-import time
-import ffmpeg
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.optim import lr_scheduler
-from torch.autograd import Variable
-import torchvision
-from extract_features import run
-from resnet import i3_res50
-import os
-
-
-def generate(datasetpath, outputpath, pretrainedpath, frequency, batch_size, sample_mode):
+def generate(framepath, labelpath, outputpath, pretrainedpath, frequency, batch_size, sample_mode):
+	# Create output directory
 	Path(outputpath).mkdir(parents=True, exist_ok=True)
 
 	# Get all frame directories
 	root_frames = Path(framepath)
 	frame_dirs = [f for f in root_frames.iterdir() if f.is_dir()]
 
-	# setup the model
+	# Setup the model
 	i3d = i3_res50(400, pretrainedpath)
 	i3d.cuda()
 	i3d.train(False)  # Set model to evaluate mode
+
 	for frame_dir in frame_dirs:
 		folder_name = frame_dir.name
 		start_time = time.time()
@@ -40,6 +24,7 @@ def generate(datasetpath, outputpath, pretrainedpath, frequency, batch_size, sam
 		np.save(os.path.join(outputpath, folder_name), features)
 		print(f"Obtained features of size: {features.shape}")
 		print(f"Done in {time.time() - start_time:.2f} seconds.")
+
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
@@ -55,8 +40,3 @@ if __name__ == '__main__':
 
 	generate(args.framepath, args.labelpath, str(args.outputpath), args.pretrainedpath,
 			 args.frequency, args.batch_size, args.sample_mode)
-
-
-##################################
-
-
